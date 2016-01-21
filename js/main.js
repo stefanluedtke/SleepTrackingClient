74c6d716dc03c63991d8111fb988c41b0c13de93
Stefan Luedtke <stefan_luedtke@gmx.net>
2016-01-21 15:35:54 +0100

    hr and accel working, max frequency

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 var accelDataPath;
+var hrDataPath;
 var ax;
 var ay;
 var az;
+var saveAccel;
 
 window.onload = function () {
 
@@ -16,81 +18,80 @@ window.onload = function () {
 
 
     //--------start of my code------------
+    //other sensors (e.g. webapis.sensorService.getDefaultSensor("LIGHT") do not work, because of old version
 
+
+    //do not go to sleep
+    tizen.power.request("CPU", "CPU_AWAKE");
+    
+    //delete all files from documents when button is pressed
+    document.getElementById("delete_btn").onclick = deleteFiles;
+    
+    
     //read acceleration values when motion event occurs
-    //TODO get average acceleration, set interval of this event
+    //TODO get average acceleration?
     window.addEventListener('devicemotion', function(e) {
-    		      ax = e.acceleration.x;
-    		      ay = e.acceleration.y;
-    		      az = e.acceleration.z;
-    		    });
+        if(saveAccel){
+        	ax = e.acceleration.x;
+        	ay = e.acceleration.y;
+        	az = e.acceleration.z;
+    		saveSensorData();
+    	}
+    });
+    
        
    //look for checked checkboxes and initialize accordingly
    var saveIntervalId;
-   document.getElementById("local_check").onchange=function(event) {
+   document.getElementById("accel_check").onchange=function(event) {
 	    var checkbox = event.target;
 	    if (checkbox.checked) {
 	        //local has been checked, initialize file and write data
-	    	console.log("initializing local file...");
+	    	console.log("initializing local file for accel data...");
 	    	initializeLocal();
-	    	saveIntervalId=setInterval(function(){ 
-	    	  saveSensorData();
-	        }, 1000);
+	    	saveAccel=true;
+//	    	saveIntervalId=setInterval(function(){ 
+//	    	  saveSensorData();
+//	        }, 1000);
 	    	
 	    } else {
 	        //local has been unchecked, stop saving
-	    	console.log("stopping local saving");
+	    	saveAccel=false;
+	    	console.log("stopping local saving of accel");
 	    	clearInterval(saveIntervalId);
 	    }
 	};
-	    
-	var sendIntervalId;
-	document.getElementById("bt_check").onchange=function(event){
-		var checkbox = event.target;
-		if(checkbox.checked){
-			//bt has been checked, initialize bt and send
-			console.log("initializing bt connection...");
-			var socket = initializeBT();
-			sendIntervalId = setInterval(function(){
-				sendSensorData(socket);	
-			}, 1000);
-			
-		}else{
-			//bt has been unchecked, stop sending
-			console.log("stopping to send data...");
-			clearInterval(sendIntervalId);
-		}
-	};
 	
-	var showIntervalId;
-	document.getElementById("show_check").onchange=function(event){
-		var checkbox = event.target;
-		if(checkbox.checked){
-			//show has been checked
-			console.log("showing data...");
-			showIntervalId = setInterval(function(){
-				showSensorData();
-			}, 1000);
-			
-		}else{
-			//show has been unchecked
-			console.log("stopping to show data...");
-			clearInterval(showIntervalId);
-		}
-	}
+	   //look for checked checkboxes and initialize accordingly
+	   var hrIntervalId;
+	   document.getElementById("hr_check").onchange=function(event) {
+		    var checkbox = event.target;
+		    if (checkbox.checked) {
+		        //local has been checked, initialize file and write data
+		    	console.log("initializing local file for hr data...");
+		    	initializeHr();
+		    	hrIntervalId=setInterval(function(){ 
+		    	  saveHrData();
+		        }, 5000);
+		    	
+		    } else {
+		        //local has been unchecked, stop saving
+		    	console.log("stopping local saving of hr");
+		    	clearInterval(hrIntervalId);
+				window.webapis.motion.stop("HRM");
+		    }
+		};
+	    	
     
 };//end onload
 
-//function that shows sensor data on screen
-showSensorData = function(){
-    document.getElementById("xaccel").innerHTML =  'X : ' +  ax;
-    document.getElementById("yaccel").innerHTML = 'Y : ' + ay;
-    document.getElementById("zaccel").innerHTML = 'Z : ' + az;
-}
 
 
-//function that saves values
+
+//function that saves accel values
 saveSensorData = function(){
+    document.getElementById("xaccel").innerHTML = "X: "+ ax;
+    document.getElementById("yaccel").innerHTML = "Y: "+ ay;
+    document.getElementById("zaccel").innerHTML = "Z: "+ az;
 	//open file and write data
 	tizen.filesystem.resolve(accelDataPath,function(file){
 		file.openStream("a", function(fs){
@@ -99,6 +100,27 @@ saveSensorData = function(){
 			fs.close();
 			}, null, "UTF-8");
 	});
+}
+
+var hrSem = false;
+saveHrData = function(){
+	//open file and write data
+	if(!hrSem){
+		hrSem=true;
+		window.webapis.motion.start("HRM", function(hrm) {
+			document.getElementById("hr").innerHTML ="HR: "+ hrm.heartRate;
+			tizen.filesystem.resolve(hrDataPath,function(file){
+				file.openStream("a", function(fs){
+					if(hrm.heartRate>0){
+						fs.write(Date.now()+","+hrm.heartRate+"\n");
+						window.webapis.motion.stop("HRM");
+						hrSem=false;
+					}
+					fs.close();
+				},null, "UTF-8");
+			});
+		});
+	}
 }
 
 //initialize local file:
@@ -117,54 +139,34 @@ initializeLocal = function(){
 	  });
 }
 
+//initialize hr file
+initializeHr = function(){
+	  //create new accelData-File with timestamp in /opt/usr/media/Documents
+	  var hrDataName = "hrData_"+Date.now()+".csv";
+	  hrDataPath = "documents/"+hrDataName;
+	  console.log("using file: "+hrDataPath);
+	  tizen.filesystem.resolve("documents", function(path){
+		  var file = path.createFile(hrDataName);
+		  file.openStream("w", function(fs){
+				//write header
+				fs.write("time,hr\n");
+				fs.close();
+				}, null, "UTF-8");
+	  });
+}
 
-//connect to bt-server
-initializeBT = function(){
-	var adapter = tizen.bluetooth.getDefaultAdapter();
-	//find device
-	adapter.discoverDevices(discoverDevicesSuccessCallback, null);
-	var discoverDevicesSuccessCallback =
-	{
-	   /* When a device is found */
-	   ondevicefound: function(device)
-	   {
-	      console.log("Found device - name: " + device.name);
-	      //TODO save device and address of device
-	   }
-	}
-	
-	//bond with device (TODO insert adress of device (or try adress of laptop: C0:F8:DA:F5:C9:1F hcitool dev)
-	adapter.createBonding("35:F4:59:D1:7A:03", onBondingSuccessCallback, onErrorCallback);
-	
-	function onBondingSuccessCallback(device)
-	{
-	   console.log("A bonding is created - name: " + device.name);
-	}
-
-	function onErrorCallback(e)
-	{
-	   console.log("Cannot create a bonding, reason: " + e.message);
-	}
-	
-	//finally connect to the service TODO use right uuid (hard-coded or from device)
-	var socket;
-	device.connectToServiceByUUID(serviceUUID, function(sock)
-			   {
-			      console.log("socket connected");
-			      socket = sock;
-			   },
-			   function(error)
-			   {
-			      console.log("Error while connecting: " + error.message);
-			   }
-			);
-	
-	//var somemsg = "test"; //was array in example
-	//socket.writeData(somemsg);
-	return socket;
+//delete all files from /opt/usr/media/Documents
+deleteFiles = function(){
+	tizen.filesystem.resolve("documents", function(dir){
+		  var onListFilesSuccess = function(files) {
+			    files.forEach(function(file) {
+			      if (!file.isDirectory) {
+			        dir.deleteFile(file.fullPath, null,null);
+			      }
+			    });
+			  };
+			  dir.listFiles(onListFilesSuccess, null);
+	});
 }
 
 
-sendSensorData = function(socket){
-	socket.writeData(Date.now()+","+ax+","+ay+","+az+"\n");
-}
